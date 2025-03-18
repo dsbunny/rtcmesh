8cbfb15598f3d24b52f0bc3705744c11c2a8e486
steve-o <fnjordy@gmail.com>
2025-03-18 20:00:24 +0000

    Release under MIT license.

--- src/rtcmesh.ts
+++ src/rtcmesh.ts
@@ -1,4 +1,7 @@
 // vim: tabstop=8 softtabstop=0 noexpandtab shiftwidth=8 nosmarttab
+// Copyright 2025 Digital Signage Bunny Corp. Use of this source code is
+// governed by an MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 export interface RTCMeshSettings {
 	label: string;
